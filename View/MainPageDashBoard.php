585d9e207f36a48cc70c3df107b514657b252a14
Asad <nazirgill1054@gmail.com>
2023-05-29 22:47:11 +0500

    Cards finallized

--- View/MainPageDashBoard.php
+++ View/MainPageDashBoard.php
@@ -86,14 +86,6 @@
 
   }
   ?>
-  <div class="card CowCard">
-    <h5 class="card-header">Cow ID</h5>
-    <div class="card-body">
-      <h5 class="card-title">Special title treatment</h5>
-      <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-      <a href="" class="btn btn-primary">Go somewhere</a>
-    </div>
-  </div>
 </div>
 </div>
 
