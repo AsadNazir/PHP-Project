a6f5a3d171dddfe677f67dcfd43d5523363eb54a
Asad Nazir <79693635+AsadNazir@users.noreply.github.com>
2023-05-30 07:27:53 +0500

    Faiza Changes

--- View/MainPageDashBoard.php
+++ View/MainPageDashBoard.php
@@ -94,29 +94,44 @@
 
   <?php
 
-  for ($i = 0; $i < 5; $i++) {
-    # code...
+  $result = $Con->DbCon->getAllCows($Con->DbCon->connection, "cows");
+
+  // var_dump($result);
+  // echo count($result);
   
 
-
+  for ($i = 0; $i < count($result); $i++) {
+    $row = $result[$i];
+    # code...
     ?>
-    <div class="card CowCard" id="<?php echo "GR-" . $i; ?>">
-      <h5 class="card-header">GR-1</h5>
+    <div class="card CowCard" id="<?php echo "GR-" . $row['id']; ?>">
+      <h5 class="card-header">GR-
+        <?php echo $row['id']; ?>
+      </h5>
       <div class="card-body CowCardBody">
-        <div class="cowImage"><img src="Images/Asset 2.svg" alt="Image"></div>
+        <div class="cowImage"><img src="Images/upload/<?php echo $row["image"]; ?>" alt="Image"></div>
         <div class="cowDetails">
-          <h5 class="card-title">Special title treatment</h5>
+          <h5 class="card-title">
+            <?php echo $row['name']; ?>
+          </h5>
           <p class="card-text CowDetails">
-            <span><strong>Height</strong> 6 ft</span>
-            <span><strong>Weight</strong> 120 kg</span>
-            <span><strong>Age</strong> 6 years</span>
+            <span><strong>Age</strong>
+              <?php echo $row['age']; ?> years
+            </span>
+            <span><strong>Weight</strong>
+              <?php echo $row['weight']; ?> kg
+            </span>
+            <span><strong>Height</strong>
+              <?php echo $row['height']; ?> ft
+            </span>
+
           </p>
         </div>
       </div>
       <div class="CowCardbtn card-body">
         <a href="" class="btn btn-primary">More Details</a>
-        <a href="" class="btn btn-secondary">Delete</a>
-        <a href="" class="btn btn-secondary">Upadte</a>
+        <a href="Delete?id=<?php echo $row['id']; ?>" class="btn btn-secondary">Delete</a>
+        <a href="Update?id=<?php echo $row['id']; ?>" class="btn btn-secondary">Update</a>
 
       </div>
     </div>
