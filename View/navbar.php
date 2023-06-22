b77c402880ea7fd609915387c9093521e97b654d
Faiza-35529 <bsef20m518@pucit.edu.pk>
2023-06-22 07:43:48 +0500

    navbar logo link corrected

--- View/navbar.php
+++ View/navbar.php
@@ -66,7 +66,7 @@
     <nav class="navbar navbar-expand-lg navbar-custom">
       <div class="container-fluid">
         <div class="logoText">
-          <a class="navbar-brand" href="#">
+          <a class="navbar-brand" href="./MainDashBoard">
             <img src="Images/Asset 2.svg" alt="Logo" class="d-inline-block align-text-top" />
             <span>Cow Automation</span>
           </a>
