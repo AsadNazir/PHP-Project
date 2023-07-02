580629baa885e77860d21d6b9fcb153104281b1b
Asad Nazir <Asad Nazir>
2023-07-02 17:49:54 +0500

    something

--- View/AddNewDietPlan.php
+++ View/AddNewDietPlan.php
@@ -36,13 +36,14 @@
         </div>
 
         <div class="submit_cont" style="margin-top:10px">
-            <button type="submit" class="mode btn submit"><span><svg style="fill: white;" xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 16 16" width="16" height="16">
-                    <path
-                        d="M11.28 6.78a.75.75 0 0 0-1.06-1.06L7.25 8.69 5.78 7.22a.75.75 0 0 0-1.06 1.06l2 2a.75.75 0 0 0 1.06 0l3.5-3.5Z">
-                    </path>
-                    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0Zm-1.5 0a6.5 6.5 0 1 0-13 0 6.5 6.5 0 0 0 13 0Z"></path>
-                </svg></span>Add the entry</button>
+            <button type="submit" class="mode btn submit"><span><svg style="fill: white;"
+                        xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="16">
+                        <path
+                            d="M11.28 6.78a.75.75 0 0 0-1.06-1.06L7.25 8.69 5.78 7.22a.75.75 0 0 0-1.06 1.06l2 2a.75.75 0 0 0 1.06 0l3.5-3.5Z">
+                        </path>
+                        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0Zm-1.5 0a6.5 6.5 0 1 0-13 0 6.5 6.5 0 0 0 13 0Z">
+                        </path>
+                    </svg></span>Add the entry</button>
             <a href="./DietPlans" class="mode btn submit">Cancel</a>
         </div>
     </form>
@@ -60,5 +61,6 @@
             this.$inputs = this.$el.find('[type="checkbox"]');
 
             this.onCheckBox();
+    )
 
-</html>
+</html >
