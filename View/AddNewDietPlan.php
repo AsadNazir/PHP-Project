b4a4640d445402393c8c9d59b192b07cbf60250c
Asad Nazir <Asad Nazir>
2023-07-02 17:53:36 +0500

    Diet Plan added

--- View/AddNewDietPlan.php
+++ View/AddNewDietPlan.php
@@ -1,16 +1,24 @@
-<!--  Purpose: Add new diet plan to the database -->
+<?php
+
+include_once("./Model/CowModal.php");
+require_once("./Model/DietModal.php");
+
+$CowModelObj = new CowModal();
+$result = $CowModelObj->getAllCows($CowModelObj->conn->connection, "cows");
+?>
+
 <div class="MainPage">
-    <form class="InputForms" id="AddNewDietPlan" enctype="multipart/form-data">
+    <form class="InputForms" id="AddNewDietPlan">
         <h1>Add Diet Plan</h1>
 
         <div class="mb-3 form-input">
-            <label for="planName" class="form-label">Diet Plane Name</label>
-            <input type="text" class="form-control" id="text" name="planName" aria-describedby="breedHelp" required />
+            <label for="breed" class="form-label">Diet Plane Name</label>
+            <input type="text" class="form-control" id="text" name="email" aria-describedby="breedHelp" required />
         </div>
 
         <div class="mb-3 form-input">
-            <label for="description" class="form-label">Description</label>
-            <input type="text" class="form-control" min="0" id="text" name="description" aria-describedby="breedHelp"
+            <label for="quantity" class="form-label">Description</label>
+            <input type="number" class="form-control" min="0" id="text" name="password" aria-describedby="breedHelp"
                 required />
         </div>
 
@@ -29,14 +37,16 @@
         </div>
 
         <div class="submit_cont" style="margin-top:10px">
-            <button type="submit" class="mode btn submit"><span><svg style="fill: white;"
-                        xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="16" height="16">
+            <button type="submit" class="btn submit"><span><svg style="fill: white;" xmlns="http://www.w3.org/2000/svg"
+                        width="20" height="16">
                         <path
                             d="M11.28 6.78a.75.75 0 0 0-1.06-1.06L7.25 8.69 5.78 7.22a.75.75 0 0 0-1.06 1.06l2 2a.75.75 0 0 0 1.06 0l3.5-3.5Z">
                         </path>
                         <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0Zm-1.5 0a6.5 6.5 0 1 0-13 0 6.5 6.5 0 0 0 13 0Z">
                         </path>
                     </svg></span>Add the entry</button>
+
+            <a href="./DietPlans" class="btn btn-danger submit">Cancel</a>
 
             <a href="./DietPlans" class="btn btn-danger submit">Cancel</a>
         </div>
@@ -130,3 +140,10 @@
                 $(document).off('click');
             }
         };
+
+        var checkboxesDropdowns = document.querySelectorAll('[data-control="checkbox-dropdown"]');
+        for (var i = 0, length = checkboxesDropdowns.length; i < length; i++) {
+            new CheckboxDropdown(checkboxesDropdowns[i]);
+        }
+    })(jQuery);
+</script>
