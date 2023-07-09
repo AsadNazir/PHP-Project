f3fcf388ecd1de9b480181be2e92f52581427095
Faiza-35529 <bsef20m518@pucit.edu.pk>
2023-07-09 22:25:39 +0500

    ..

--- View/Chart.php
+++ View/Chart.php
@@ -2,23 +2,23 @@
   <div>
     <h1>Cow Population</h1>
     <canvas class="Chart" id="myChart"></canvas>
-    <div class="ChartBtnDiv" >
-    <button class="PrintBtn btn" id="0">Print Chart</button>
+    <div class="ChartBtnDiv">
+      <button class="PrintBtn btn" id="0">Print Chart</button>
     </div>
     <h1>Milk Production</h1>
     <canvas class="Chart" id="myChart1"></canvas>
-    <div class="ChartBtnDiv" >
-    <button class="PrintBtn btn" id="1">Print Chart</button>
+    <div class="ChartBtnDiv">
+      <button class="PrintBtn btn" id="1">Print Chart</button>
     </div>
     <h1>Feed</h1>
     <canvas class="Chart" id="myChart2"></canvas>
-    <div class="ChartBtnDiv" >
-    <button class="PrintBtn btn" id="2">Print Chart</button>
+    <div class="ChartBtnDiv">
+      <button class="PrintBtn btn" id="2">Print Chart</button>
     </div>
     <h1>Breed</h1>
     <canvas class="Chart" id="myChart3"></canvas>
-    <div class="ChartBtnDiv" >
-    <button class="PrintBtn btn" id="3">Print Chart</button>
+    <div class="ChartBtnDiv">
+      <button class="PrintBtn btn" id="3">Print Chart</button>
     </div>
     <canvas id="myChart4"></canvas>
   </div>
@@ -28,5 +28,3 @@
 <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.0/jspdf.umd.min.js" integrity="sha512-6Yq06JzZZPm8X7sPLkFApbkzVhEKsFonD8eeNFezGevKUwQOo48nY27pdIAJGnEoyfYjtTOiM6v+aQofuXqKgw==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script> -->
 <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.5.1/jspdf.umd.min.js" integrity="sha512-qZvrmS2ekKPF2mSznTQsxqPgnpkI4DNTlrdUmTzrDgektczlKNRRhy5X5AAOnx5S09ydFYWWNSfcEqDTTHgtNA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script> -->
-
-
